f6b145427969717ba80620684e533e6a80386c31
ggovindan <ggovindan@thuuz.com>
2020-09-20 19:47:39 -0700

    Added a non-recursive approach to the flatten_array challenge and added some profiling

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 export PYTHONPATH=${SCRIPT_DIR}/src:${SCRIPT_DIR}/test
 
-pytest -v test/
+pytest -vv --durations=5 test/
--- src/flatten_array.py
+++ src/flatten_array.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from typing import List
+from memory_profiler import profile
 
 """
 # I took a recursive approach to the problem as it is simple to visualize.
@@ -9,7 +10,7 @@ from typing import List
 
 """
 
-
+@profile(precision=4)
 def flatten_array(l: List, result: List = []):
     for item in l:
         if isinstance(item, (list, tuple)):
@@ -19,6 +20,32 @@ def flatten_array(l: List, result: List = []):
     print(result)
 
 
+@profile(precision=4)
+def flatten_array_non_recursive(l: List, result: List = []):
+    for item in l:
+        if isinstance(item, (list, tuple)):
+            stack = []
+            stack.extend(item)
+            for x in stack:
+                if isinstance(x, (list, tuple)):
+                    stack.extend(x)
+                else:
+                    result.append(x)
+        else:
+            result.append(item)
+    return result
+
+
+
+
 if __name__ == "__main__":
-    x = [1, [2], [[3]], [4, [[5]]]]
+    # x = [1, [2], [[3]], [4, [[5]]]]
+    # x = [[1, 2, 3, 4, 5, 6, 7, 8, 9], (10, 11, 12), [13, [14, 15, 16]], [17, 18]]
+    x = [[[[[[[[[[[[[[[[0]]]],0]]]]]]]]]]]]
+    start = datetime.utcnow()
     flatten_array(x)
+    print("recursive time={}".format(datetime.utcnow() - start))
+    start = datetime.utcnow()
+    print(flatten_array_non_recursive(x))
+    print("iterative time={}".format(datetime.utcnow() - start))
+
